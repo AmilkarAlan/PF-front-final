f93141ba8bcd49b75b68ee2c8568231512ff5e1e
Amilkar Sanchez <amilkaralan@gmail.com>
2024-05-08 17:46:07 -0600

    commit to vercel

--- src/Components/Logout.js
+++ src/Components/Logout.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logOutbuttonstyles from './module.Logout.css';
 
 function Logout() {
+    const accessToken = localStorage.getItem("accessToken");
+    const [ name, setName ] = useState("")
+
+    useEffect(() => {
+        if (accessToken === null) {
+            return setName("Sign in");
+        }
+        return setName("Logout");
+    }, [ accessToken, name ]);
 
     const handleLogout = () => {
-            
         localStorage.removeItem('accessToken');
         localStorage.removeItem('refreshToken');
-
         window.location.href = '/login'
     };
-    
+
+    const handleLogin = () => {
+        window.location.href = '/login'
+    };
+
 
     return (
-        <button className="Logout" onClick={handleLogout}>Logout</button>
+        <button className="Logout" onClick={ name === "Logout" ? handleLogout : handleLogin }>{ name }</button>
 
     )
 
